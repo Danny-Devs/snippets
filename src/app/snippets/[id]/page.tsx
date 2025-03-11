991d555c0e2068964c5f216117d41c7605c66afe
Daniel Ahn <et3rnal.d@gmail.com>
2025-03-10 23:24:02 -0600

    save point

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation';
 import { db } from '@/db';
+import Link from 'next/link';
 
 interface SnippetShowPageProps {
   params: Promise<{ id: string }>;
@@ -17,8 +18,19 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
 
   return (
     <div>
-      <h1>{snippet.title}</h1>
-      <pre>{snippet.code}</pre>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+
+        <div className="flex gap-4">
+          <Link href={`/snippets/${id}/edit`} className="border p-2 rounded">
+            Edit
+          </Link>
+          <button className="border p-2 rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
     </div>
   );
 }
